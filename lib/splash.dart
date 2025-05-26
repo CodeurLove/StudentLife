e73072c47193df4fecbd86e1cf67767538e22861
CodeurLove <aimebongo64@gmail.com>
2025-05-26 14:52:55 +0000

    Ajout du splash screen, des assets et mise à jour pubspec/main

---
+++ lib/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF4F1FF), // fond clair
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/eduria.png', // Assurez-vous que le chemin de l'image est correct
+              width: 200, // Ajustez la taille selon vos besoins
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
