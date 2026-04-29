7b0cf96bfda9c6cc8429220da5ffbf60b36c0dea
CodeurLove <aimebongo64@gmail.com>
2026-04-29 22:07:25 +0000

    modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:eduria/splash.dart';
 import 'package:flutter_locales/flutter_locales.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Locales.init(
     ['fr', 'en', 'es', 'de', 'it', 'pt', 'zh', 'ja', 'ko', 'ru', 'ar'],
   );
@@ -13,7 +18,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return LocaleBuilder(
@@ -23,11 +27,10 @@ class MyApp extends StatelessWidget {
           fontFamily: 'jura',
           textTheme: const TextTheme(
             bodyMedium: TextStyle(
-              //color: Color(0xFF3F3A54), // Couleur de texte sombre
               fontSize: 30,
             ),
           ),
-          scaffoldBackgroundColor: const Color(0xFFFFF3FE), // fond clair
+          scaffoldBackgroundColor: const Color(0xFFFFF3FE),
         ),
         home: SplashScreen(),
         localizationsDelegates: Locales.delegates,
